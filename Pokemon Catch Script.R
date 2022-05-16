c68ce0ea86705c9b2425cf86feb03c41a732dd5b
plubgoard <99295621+plubgoard@users.noreply.github.com>
2022-05-15 23:31:59 -0500

    Update Pokemon Catch Script.R

--- Pokemon Catch Script.R
+++ Pokemon Catch Script.R
@@ -1,5 +1,6 @@
 # read relevant files
-pokemon <- read.csv('Pokemon.csv', stringsAsFactors = T,head = T, row.names = 1)
+pokemon <- read.csv('BaseCatchRate.csv', stringsAsFactors = T,head = T, row.names = 1)
+berr <- c(1, 1.5, 1.8, 2.5)
 
 # gather input from user on pokemon and what berries they are using
 v.p <- readline(prompt="Enter Pokemon number: ")
@@ -8,7 +9,7 @@ v.berry <- readline(prompt="Berry (None = 1, Razz = 2, Silver = 3, Golden = 4): 
 # do preliminary variable assignment
 v.p <- as.integer(v.p)
 v.berry <- as.integer(v.berry)
-berry = mod[v.berry]
+berry <- berr[v.berry]
 BCR <- pokemon[v.p,1]
 flee_chance <- pokemon[v.p,2]
 
@@ -19,7 +20,7 @@ probCatch <- function(BCR,ball,berry,throw) {
   return(p)
 }
 
-# calculates the number of throws before 80% chance that a catch is made
+# calculates the number of t1hrows before 80% chance that a catch is made
 throwsCatch <- function(pcatch) {
   return(qgeom(.8, pcatch))
 }
@@ -30,47 +31,45 @@ throwsFlee <- function(p) {
 }
 
 # CREATES VECTORS FOR PROBABILITY OF CATCH:
-Pball <- c(probCatch(BCR,1,berry,1)[1,1],
-           probCatch(BCR,1,berry,1.15)[1,1],
-           probCatch(BCR,1,berry,1.5)[1,1],
-           probCatch(BCR,1,berry,1.85)[1,1]) #[normal, nice, great, excellent]
+Pball <- c(probCatch(BCR,1,berry,1),
+           probCatch(BCR,1,berry,1.15),
+           probCatch(BCR,1,berry,1.5),
+           probCatch(BCR,1,berry,1.85)) #[normal, nice, great, excellent]
 
-Gball <- c(probCatch(BCR,1.5,berry,1)[1,1],
-           probCatch(BCR,1.5,berry,1.15)[1,1],
-           probCatch(BCR,1.5,berry,1.5)[1,1],
-           probCatch(BCR,1.5,berry,1.85)[1,1]) #[normal, nice, great, excellent]
+Gball <- c(probCatch(BCR,1.5,berry,1),
+           probCatch(BCR,1.5,berry,1.15),
+           probCatch(BCR,1.5,berry,1.5),
+           probCatch(BCR,1.5,berry,1.85)) #[normal, nice, great, excellent]
 
-Uball <- c(probCatch(BCR,2,berry,1)[1,1],
-           probCatch(BCR,2,berry,1.15)[1,1],
-           probCatch(BCR,2,berry,1.5)[1,1],
-           probCatch(BCR,2,berry,1.85)[1,1]) #[normal, nice, great, excellent]
+Uball <- c(probCatch(BCR,2,berry,1),
+           probCatch(BCR,2,berry,1.15),
+           probCatch(BCR,2,berry,1.5),
+           probCatch(BCR,2,berry,1.85)) #[normal, nice, great, excellent]
 
 # creates table for catch probability
-catch_cbind <- cbind(Pball,Gball,Uball)
-catchTab <- catch_cbind
+catchTab <- cbind(Pball,Gball,Uball)
 colnames(catchTab) <- c('POKEBALL', 'GREATBALL', 'ULTRABALL')
 rownames(catchTab) <- c('NORMAL', 'NICE', 'GREAT', 'EXCELLENT')
 catchTab <- as.table(catchTab)
 
 # CREATES VECTORS FOR NUMBER OF THROWS BEFORE CATCH
-throwsP <- c(throwsCatch(probCatch(BCR,1,berry,1)[1,1]),
-             throwsCatch(probCatch(BCR,1,berry,1.15)[1,1]),
-             throwsCatch(probCatch(BCR,1,berry,1.5)[1,1]),
-             throwsCatch(probCatch(BCR,1,berry,1.85)[1,1])) #[normal, nice, great, excellent]
+throwsP <- c(throwsCatch(probCatch(BCR,1,berry,1)),
+             throwsCatch(probCatch(BCR,1,berry,1.15)),
+             throwsCatch(probCatch(BCR,1,berry,1.5)),
+             throwsCatch(probCatch(BCR,1,berry,1.85))) #[normal, nice, great, excellent]
 
-throwsG <- c(throwsCatch(probCatch(BCR,1.5,berry,1)[1,1]),
-             throwsCatch(probCatch(BCR,1.5,berry,1.15)[1,1]),
-             throwsCatch(probCatch(BCR,1.5,berry,1.5)[1,1]),
-             throwsCatch(probCatch(BCR,1.5,berry,1.85)[1,1])) #[normal, nice, great, excellent]
+throwsG <- c(throwsCatch(probCatch(BCR,1.5,berry,1)),
+             throwsCatch(probCatch(BCR,1.5,berry,1.15)),
+             throwsCatch(probCatch(BCR,1.5,berry,1.5)),
+             throwsCatch(probCatch(BCR,1.5,berry,1.85))) #[normal, nice, great, excellent]
 
-throwsU <- c(throwsCatch(probCatch(BCR,2,berry,1)[1,1]),
-             throwsCatch(probCatch(BCR,2,berry,1.15)[1,1]),
-             throwsCatch(probCatch(BCR,2,berry,1.5)[1,1]),
-             throwsCatch(probCatch(BCR,2,berry,1.85)[1,1])) #[normal, nice, great, excellent]
+throwsU <- c(throwsCatch(probCatch(BCR,2,berry,1)),
+             throwsCatch(probCatch(BCR,2,berry,1.15)),
+             throwsCatch(probCatch(BCR,2,berry,1.5)),
+             throwsCatch(probCatch(BCR,2,berry,1.85))) #[normal, nice, great, excellent]
 
 # creates table for number of throws before catch
-throwsmtx <- cbind(throwsP, throwsG, throwsU)
-throwsTab <- throwsmtx
+throwsTab <- cbind(throwsP, throwsG, throwsU)
 colnames(throwsTab) <- c('Pokeball', 'Greatball', 'Ultraball')
 rownames(throwsTab) <- c('Normal', 'Nice', 'Great', 'Excellent')
 throwsTab <- as.table(throwsTab)
